0ba4f4d3a88daba68aacf18773f995056ff50f4d
Eugeny Vlasov <eugeny.vlasov@gmail.com>
2025-10-07 23:31:13 +0300

    tests: add basic tests (xUnit) for Dictionary

---
+++ ConsoleDictionary.tests/DictionaryManagerTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleDictionary.Helpers;
+using ConsoleDictionary.Entities;
+using ConsoleDictionary.Managers;
+
+
+namespace ConsoleDictionary.tests
+{
+    public class DictionaryManagerTests
+    {
+        [Fact]
+        public void AddWord_ShouldIncreaseCount()
+        {
+            var manager = new DictionaryManager(new List<Word>());
+            manager.AddWord(new Word("apple", new List<string> { "яблоко" }));
+
+            Assert.Single(manager.GetAllWords());
+        }
+
+        [Fact]
+        public void DeleteWord_ShouldRemoveWord()
+        {
+            var manager = new DictionaryManager(new List<Word> { new Word("apple", new List<string> { "яблоко" }) });
+            manager.DeleteWord("apple");
+
+            Assert.Empty(manager.GetAllWords());
+        }
+
+        [Fact]
+        public void FindWord_ShouldFindWord()
+        {
+            var manager = new DictionaryManager(new List<Word> { new Word("apple", new List<string> { "яблоко" }) });
+            Assert.Equal("apple", manager.FindWord("apple").Text);
+        }
+    }
+}
---
+++ ConsoleDictionary.tests/TrainerTests.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleDictionary.Entities;
+using ConsoleDictionary.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDictionary.tests
+{
+    public class TrainerTests
+    {
+        [Fact]
+        public void CheckAnswer_ShouldReturnTrue_IfTranslationMatches()
+        {
+            var word = new Word("apple", new List<string> { "яблоко" });
+            var manager = new DictionaryManager(new List<Word> { word });
+            var trainer = new Trainer(manager);
+
+            bool result = trainer.CheckAnswer(word, "яблоко");
+
+            Assert.True(result);
+            Assert.Equal(1, word.CorrectCount);
+        }
+    }
+}
