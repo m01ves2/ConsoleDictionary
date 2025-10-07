47b85dbfd6e5f8e808cd37aed1f7c6d63305ff7e
Eugeny Vlasov <eugeny.vlasov@gmail.com>
2025-10-07 22:57:56 +0300

    refactor: add ConsoleHelper.cs for colorful Console Output

---
+++ ConsoleDictionary/Helpers/ConsoleHelper.cs
@@ -0,0 +1,60 @@
+﻿namespace ConsoleDictionary.Helpers
+{
+    public static class ConsoleHelper
+    {
+        /// <summary>
+        /// Prints a message in a specified color, then restores the original color.
+        /// </summary>
+        public static void Print(string message, ConsoleColor color)
+        {
+            var oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ForegroundColor = oldColor;
+        }
+
+        /// <summary>
+        /// Prints a message in a specified color without a line break.
+        /// </summary>
+        public static void PrintInline(string message, ConsoleColor color)
+        {
+            var oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(message);
+            Console.ForegroundColor = oldColor;
+        }
+
+        /// <summary>
+        /// Prints a success message in green.
+        /// </summary>
+        public static void PrintSuccess(string message)
+        {
+            Print(message, ConsoleColor.Green);
+        }
+
+        /// <summary>
+        /// Prints an error message in red.
+        /// </summary>
+        public static void PrintError(string message)
+        {
+            Print(message, ConsoleColor.Red);
+        }
+
+        /// <summary>
+        /// Prints a warning message in yellow.
+        /// </summary>
+        public static void PrintWarning(string message)
+        {
+            Print(message, ConsoleColor.Yellow);
+        }
+
+        /// <summary>
+        /// Prints usual message in default color.
+        /// </summary>
+        /// <param name="message"></param>
+        public static void PrintNormal(string message)
+        {
+            Print(message, Console.ForegroundColor);
+        }
+    }
+}
